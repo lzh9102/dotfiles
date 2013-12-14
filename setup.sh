1d00f2019e72e23b01ea9ce3eae9eb52f2f36074
Che-Huai Lin <lzh9102@gmail.com>
2013-12-14 15:49:16 +0800

    Add makegen script

--- setup.sh
+++ setup.sh
@@ -29,6 +29,7 @@ FreeBSD   .zsh/zshrc.system zsh/zshrc.freebsd
 * .bin/ack <ack> 755
 * .bin/cloc <cloc> 755
 * .bin/interactive-rename <imv> 755
+* .bin/makegen <mkgen> 755
 """
 
 # fields: id url sha1
@@ -39,6 +40,7 @@ URLS="""
 ack http://beyondgrep.com/ack-2.10-single-file 6052cee5a4f580006fb9135e46411c5322c24a2a
 cloc http://sourceforge.net/projects/cloc/files/latest/download?source=files d002e85b7deb988e4f88eaf3f9697cf011ec0c8a
 imv https://raw.github.com/lzh9102/interactive-rename/master/interactive-rename.py
+mkgen https://github.com/lzh9102/makegen/blob/master/makegen.py
 """
 
 # non-builtin programs that this script depends on
